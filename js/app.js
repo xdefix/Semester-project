9f1a261bc496eb03bcdc4a8dc5a2c6a8b746745a
Maria-Mihaela <mms.marinova@gmail.com>
2026-05-15 11:06:45 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const window,BASE_PATH = "/Semester-project/";
+const BASE_PATH = "/Semester-project/";
 import { renderLanding } from "./landing.js";
 import { renderDownload } from "./download.js";
 import { renderIntro } from "./intro.js";
